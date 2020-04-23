21207a1474ab494daa5bd8b4e815d70ac4d93e6c
ncrivelli <63763410+ncrivelli@users.noreply.github.com>
2020-04-22 23:37:30 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,12 @@
+/*	"Hello World": Programa que muestra por consola la frase 'Hola mundo'.
+	Autor: Nicolas Emmanuel Crivelli
+	Fecha: 22/04/2020
+*/
+
+#include <iostream>
+ 
+int main () 
+{
+    std::cout << "Hola mundo";
+    return 0;
+}
