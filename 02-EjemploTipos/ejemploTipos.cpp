2e6035de366606c35f319e8ad73500116bdc2fda
ncrivelli <nicocrivelli@est.frba.utn.edu.ar>
2020-05-20 20:09:34 -0300

    Subida de archivo ejemploTipos.cpp

---
+++ 02-EjemploTipos/ejemploTipos.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <assert.h>
+#include <string.h>
+
+int main(){
+    
+    int numero = 9;
+    int numero2 = -4;
+    
+    double grande = 1231452352355;
+    double grande2 = 77682910921;
+    
+    bool positivo = false;
+    bool negativo = true;
+    
+    float real = 8.25;
+    float real2 = 2.00;
+    
+    std::string cadena = "Frase del tipo 'String'.";
+    std::string cadena2 = "Otra frase mas";
+    
+    char simbolo = 'a';
+    char simbolo2 = '+';
+    
+    unsigned numPositivo = 324;
+    unsigned numPositivo2 = 4434;
+    
+    int suma = numero + numero2;
+    assert(suma>0);
+    positivo = true;
+    negativo = false;
+    
+    float cociente = real / real2;
+    assert(cociente>4);
+    
+    std::string frase = cadena + cadena2;
+    assert(frase=="Frase del tipo 'String'.Otra frase mas");
+    
+    return 0;
+}
